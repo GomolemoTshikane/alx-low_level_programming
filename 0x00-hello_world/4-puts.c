2f618666a3256305b0bd409b8e916240c1a55d12
GomolemoTshikane <gktshikane@gmail.com>
2023-03-16 14:00:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print string of characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	
+	return (0);
+}
